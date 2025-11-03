2323a8013e745d9861f315d206b248d8a1c11c84
Pratham Bahuguna <pbahu@uic.edu>
2025-11-03 01:52:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Welcome to Spotify!")
+
+let song=[
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Let me love you", filepath:"song/1.mp3", coverPath:"covers/1.jpg"},
+]
+
+let audioElement=new Audio('1.mp3');
+audioElement.play();
+
+//listen to events
+
